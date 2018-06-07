b02267c66e4ad193f5f56afd1f56ba0a0911bd41
Mikołaj Klonowski <nick-93@o2.pl>
2018-06-07 22:27:53 +0200

    Zadanie 13 jedna petlna

--- src/SprawdzenieLiczby.java
+++ src/SprawdzenieLiczby.java
@@ -5,52 +5,30 @@ public class SprawdzenieLiczby {
         Scanner sc = new Scanner(System.in);
         System.out.println("Podaj Liczbe:");
         int a = sc.nextInt();
-        if (a > 100 && a < 200 && a % 3 == 0) {
-            System.out.println("Podana liczba jest ok");
+        while (a < 100 || a > 200 || a % 3 != 0) {
+
+            if (a > 100 && a < 200 && a % 3 == 0) {
+                System.out.println("Podana liczba jest ok");
+            } else if (a >= 100 && a <= 200 && a % 3 != 0) {
+                System.out.println("Podana liczba nie jest podzielna przez 3");
 
 
-        } else if (a >= 100 && a <= 200 && a % 3 != 0) {
-            System.out.println("Podana liczba nie jest podzielna przez 3");
-            while (a >= 100 && a <= 200 && a % 3 != 0){
-                System.out.println("Podaj liczbe:");
-                a = sc.nextInt();}
+            } else if (a < 100) {
+                System.out.println("Podana liczba jest za mała");
+
+            } else if (a > 200) {
+                System.out.println("Podana liczba jest za duża");
+            }
 
 
-        } else if (a < 100) {
-            System.out.println("Podana liczba jest za mała");
-            while (a<100){
-                System.out.println("Podaj liczbe:");
-                a = sc.nextInt();}
-
-        } else if (a > 200) System.out.println("Podana liczba jest za duża");
-        while (a>=200){
-            System.out.println("Podaj liczbe:");
-            a = sc.nextInt();}
+            System.out.println("Podaj Liczbe:");
+             a = sc.nextInt();
+        }
     }
-
 }
 
 
-//        do {
-//            System.out.println("Podaj Liczbe:");
-//             a = sc.nextInt();
-//            if (a > 100 && a < 200 && a % 3 == 0) {
-//                System.out.println("Podana liczba jest ok");
-//                break;
-//
-//
-//            } else if (a >= 100 && a <= 200 && a % 3 != 0) {
-//                System.out.println("Podana liczba nie jest podzielna przez 3");
-//
-//            } else if (a < 100) {
-//                System.out.println("Podana liczba jest za mała");
-//
-//            } else if (a > 200) System.out.println("Podana liczba jest za duża");
-//        }
 
-//
-//    }
-//
-//
-//}
+
+
 
